1ee075008d9f33b1f2ea6275c35edc9cc1414f10
esolo <solodovnyk@gmail.com>
2022-10-17 16:01:40 +0300

    polymorphic example one to one

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        dump(Post::class);
+        $post = Post::find(1);
+        dump($post->getAttributes());
+
+        dump($post->image->getAttributes());
+
+        echo '<br><br>';
+        dump(User::class);
+        $user = User::find(1);
+        dump($user->getAttributes());
+
+        dump($user->image->getAttributes());
+
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Image;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +17,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        //User::factory(10)->has(Image::factory()->count(2))->create();
+        User::factory(10)->create()->each(function ($item) {
+            Image::factory()->for($item, 'imageable')->count(rand(1,3))->create();
+        });
+
+        //Post::factory(10)->has(Image::factory()->count(2))->create();
+        Post::factory(10)->create()->each(function ($item) {
+            Image::factory()->for($item, 'imageable')->count(rand(1,3))->create();
+        });
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
